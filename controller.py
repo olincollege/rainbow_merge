7fd44e31363cfe6c7b2a3fb1e7aaa8f614b3d8e8
wyoung1810 <144073384+wyoung1810@users.noreply.github.com>
2024-04-21 20:22:41 -0400

    Update controller.py

--- controller.py
+++ controller.py
@@ -3,8 +3,12 @@ class controller:
     def help:
     def get_controls:
 
-class human_controller(controller):
+class p1_controller(controller):
     def __init__():
         keyboard_controls  # ( <- ->/ A D)
     def get_user_input():
-    
+
+class p2_controller(controller):
+    def __init__():
+        keyboard_controls  # ( <- ->/ A D)
+    def get_user_input():
