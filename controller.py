3b66e945479bf5d24b07f59a0c36ddf8cea64ff9
Rainier Hardjanto <65476882+rainiercs50@users.noreply.github.com>
2024-04-21 20:46:04 -0400

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,12 +1,17 @@
+import pygame 
+
 class controller:
+    
     def restart:
     def help:
     def get_controls:
 
 class p1_controller(controller):
     def __init__():
-        keyboard_controls  # ( <- ->/ A D)
+        keyboard_controls = ['A', 'D', 'S']   # ( <- ->/ A D)
     def get_user_input():
+        keys = pygame.key.get_pressed()
+        
 
 class p2_controller(controller):
     def __init__():
