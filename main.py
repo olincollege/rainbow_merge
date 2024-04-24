17a13b673e67cd8179e4a11ea7b8307baae516d4
Will Young <ukjoma@gmail.com>
2024-04-24 15:06:41 -0400

    Outline for main file and minor edits to animals and subgame files to ensure they will work with main file

--- main.py
+++ main.py
@@ -1,22 +1,89 @@
 import sys
 import pygame
 from pygame.locals import *
-import subgame
+from subgame import Subgame
+from controller import P1Controller
 
 
 def main():
     pygame.init()
 
-    x = subgame.Subgame()
+    subgame = Subgame()
+    controller = P1Controller()
 
     while True:
+
+        pygame.display.update()
+
+        # draw background
+        subgame.draw_board(subgame.subgame_board)
+
+        # draw all animals
+
+        # check for collisions with other animals
+
+        # move dropped animals
+
+        # move hanging animal
+
+        # refresh window
+        pygame.display.update()
+
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-                break
-        pygame.display.update()
-        x.draw_board(x.subgame_board)
 
 
 main()
+
+
+"""
+while True:
+        # pygame management
+        clock.tick(60)
+        events = pygame.event.get()
+
+        # draw features of level
+        game.draw_level_background(board)
+        game.draw_board(board)
+        if gates:
+            game.draw_gates(gates)
+        game.draw_doors(doors)
+
+        # draw player
+        game.draw_player([magma_boy, hydro_girl])
+
+        # move player
+        arrows_controller.control_player(events, magma_boy)
+        wasd_controller.control_player(events, hydro_girl)
+
+        game.move_player(board, gates, [magma_boy, hydro_girl])
+
+        # check for player at special location
+        game.check_for_death(board, [magma_boy, hydro_girl])
+
+        game.check_for_gate_press(gates, [magma_boy, hydro_girl])
+
+        game.check_for_door_open(fire_door, magma_boy)
+        game.check_for_door_open(water_door, hydro_girl)
+
+        # refresh window
+        game.refresh_window()
+
+        # special events
+        if hydro_girl.is_dead() or magma_boy.is_dead():
+            show_death_screen(game, controller, level)
+
+        if game.level_is_done(doors):
+            show_win_screen(game, controller)
+
+        if controller.press_key(events, K_ESCAPE):
+            show_level_screen(game, controller)
+
+        # close window is player clicks on [x]
+        for event in events:
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+"""
