bd2806b65922171f77850ef6ad98e4a0e6ed3b0d
Rainier Hardjanto <rainierwardhana@gmail.com>
2024-05-01 04:03:34 -0400

    Docstrings for animals.py
    All docstrings have been done for this file

--- animals.py
+++ animals.py
@@ -6,6 +6,14 @@ from board import Board
 
 class Block:
     def __init__(self, x_position, y_position, n=0):
+        """
+        Initialize a Block instance with specified color index, and x and y positions.
+
+        Parameters:
+            x_position (int): The x-coordinate where the block will be placed on the screen.
+            y_position (int): The y-coordinate where the block will be placed on the screen.
+            n (int): The color index, used to determine the block's color from a predefined list.
+        """
         self.colors = [
             # B50303
             (181, 3, 3),
@@ -36,10 +44,21 @@ class Block:
         self.y_position = y_position
 
     def get_color(self):
+        """
+        Get the current color of the block.
+
+        Returns:
+            tuple: The RGB tuple representing the color of the block.
+        """
         return self.color
 
     def draw(self, screen):
-        # 60x60 pixel rectangle
+        """
+        Draw the block on a specified Pygame screen as a 60x60 pixel rectangle.
+
+        Parameters:
+            screen: The Pygame display surface to draw the block on.
+        """
         pygame.draw.rect(
             screen, self.color, pygame.Rect(self.x_position, self.y_position, 60, 60)
         )
@@ -88,7 +107,18 @@ class Block:
         y_position,
         blocks_list,
     ):
-        """Moves the block to the lowest possible space that it can fill."""
+        """
+        Makes the block fall to the next available space directly below it, if possible.
+
+        Parameters:
+            x_position (int): The x-coordinate index of the block in the grid.
+            y_position (int): The y-coordinate index of the block in the grid.
+            blocks_list (list of lists): 2D list representing the grid of blocks.
+
+        Returns:
+            bool: True if the block was able to fall, False otherwise.
+        """
+
         # vertical merges
         if y_position < 7:
             # check if there is a block below the current one
@@ -146,6 +176,23 @@ class Block:
 
 class MovingAnimal:
     def __init__(self, player_num, controller):
+        """
+        Initializes a MovingAnimal instance, which represents a player-controlled element that can drop blocks onto a grid.
+
+        Parameters:
+            player_num (int): The number identifying the player. This number determines certain behaviors like controller setup.
+            controller: The input mechanism associated with the player. This can be a keyboard, gamepad, etc., specific to player one.
+
+        Attributes:
+            colors (list of tuple): A list of RGB color tuples used to visualize the animal.
+            n (int): Randomly selected initial color index for the animal.
+            possible_x_positions (list of int): Fixed x-coordinates where the animal can move horizontally.
+            possible_y_positions (list of int): Fixed y-coordinates where blocks can be dropped.
+            color (tuple): Current color of the animal determined by `n`.
+            player_num (int): Player identifier.
+            controller: Control mechanism for the player.
+            player_position (int): Current horizontal position index of the animal.
+        """
         self.colors = [
             # B50303
             (181, 3, 3),
@@ -178,6 +225,14 @@ class MovingAnimal:
         self.player_position = 6
 
     def move_animal(self, player_input, blocks_list):
+        """
+        Processes player input to move the animal left, right, or drop a block at the lowest possible unoccupied position in the column.
+
+        Parameters:
+            player_input (str): The input command from the player ('left', 'right', or 'drop') indicating the desired action.
+            blocks_list (list of lists): The grid where blocks are placed, used to check for available spaces when dropping blocks.
+
+        """
         if player_input == "right":
             if self.player_position == 7:
                 self.player_position = 0
@@ -207,5 +262,11 @@ class MovingAnimal:
                 self.color = self.colors[self.n]
 
     def draw_moving_animals(self, screen):
+        """
+        Draws the moving animal on the provided Pygame screen at its current position using its current color.
+
+        Parameters:
+            screen: The Pygame display surface where the animal will be drawn as a 60x60 pixel rectangle at a position determined by the current player position.
+        """
         pos = self.possible_x_positions[self.player_position]
         pygame.draw.rect(screen, self.color, pygame.Rect(pos, 30, 60, 60))
